d711b2f7306951545d2ad9c4f9e980019032a1ae
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-05-20 04:01:39 +0100

    cors

--- IWitness/settings.py
+++ IWitness/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://i-witness.up.railway.app/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["i-witness-be.onrender.com", "localhost", "127.0.0.1", 'watchdog-beige.vercel.app']
 
 
 # Application definition
